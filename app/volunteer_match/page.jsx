e2c4b2ac3c07c6257edbba95b8c57a91703932bb
ahad-adesanya <ahad.a.adesanya@gmail.com>
2024-09-19 22:54:08 -0500

    create start volunteer

---
+++ app/volunteer_match/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function match_page() {
+  return (
+    <body>
+      <header>
+        <nav>
+            
+            <h1>Project Xenia</h1>
+
+            <ul id="categories">
+              <li><a href="home.html">Home</a></li>
+              <li><a href="entry.html">Sign Out</a></li>
+            </ul>
+        </nav>
+      </header>
+      
+
+  
+    </body>
+  );
+}
