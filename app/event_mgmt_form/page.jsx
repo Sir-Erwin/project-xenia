6fdb7a3dabbcf5f83f30ab077612142974d43af9
jashourr <joshuleerod@gmail.com>
2024-09-20 15:46:10 -0500

    added some basic css styling- still need to refine more to match website theme

--- app/event_mgmt_form/page.jsx
+++ app/event_mgmt_form/page.jsx
@@ -1,12 +1,14 @@
 // import Image from "next/image";
 import './event_mgmt_form.css';
+import NavBar from "../navbar";
 
 const eventMgmtForm = () => {
     return (
         <div id = 'eventMgmt'>
+            <NavBar/>
+            
             <h1>Event Management</h1>
-            <hr/>
-            <form id = 'event_form'>
+            <form id = 'event_form' action="Submit" method="post">
 
                 <div className="inputbox"> {/* Textbox for event name with max length of 100 characters */}
                     <label for='eventNameInput'>Event Name:</label>
@@ -16,32 +18,36 @@ const eventMgmtForm = () => {
 
                 <div className="inputbox"> {/* Textarea for event desc */}
                     <label for='eventDescrip'>Event Description:</label>
-                    <textarea id="eventDescrip" name='eventDescrip' rows="4" cols='50' required>
-                        Short Description of Event
+                    <textarea id="eventDescrip" name='eventDescrip' rows="4" cols='50' 
+                        placeholder='Short Description of Event' required>
+                        
                     </textarea>
                 </div>
 
                 <div className="inputbox"> {/* Textarea for event location */}
                     <label for='eventLoc'>Location:</label>
-                    <textarea id="eventLoc" name='eventLoc' rows="4" cols='50' required>
-                        Short Description of Event
+                    <textarea id="eventLoc" name='eventLoc' rows="4" cols='50' 
+                        placeholder='Location of Event' required>
+                        
                     </textarea>
                 </div>
 
                 <div className='inputbox'> {/* Dropdown menu for multiple skills */}
                     <label for="reqSkills">Required Skills:</label>
-                    <select data-placeholder='Select' name="reqSkills" id="reqSkills" multiple class="chosen-select" required>
-                        <option value=""></option>
-                        <option value="sk1">Skill 1</option>
-                        <option value="sk2">Skill 2</option>
-                        <option value="sk3">Skill 3</option>
-                        <option value="sk4">Skill 4</option>
+                    <select size = '2' data-placeholder='Select' name="reqSkills" id="reqSkills" 
+                        multiple class="chosen-select" style={{color:'#ccc'}} required>
+                            <option value='null'>Select Skills</option>
+                            <option value="sk1">Skill 1</option>
+                            <option value="sk2">Skill 2</option>
+                            <option value="sk3">Skill 3</option>
+                            <option value="sk4">Skill 4</option>
                     </select>
                 </div>
 
                 <div className='inputbox'> {/* Dropdown menu for single selection of urgency */}
                     <label for="urg">Urgency:</label>
-                    <select name="urg" id="urg" required>
+                    <select name="urg" id="urg" style={{color:'#000'}} required>
+                        <option value="null"></option>
                         <option value="high">High</option>
                         <option value="med">Med</option>
                         <option value="low">Low</option>
@@ -50,7 +56,12 @@ const eventMgmtForm = () => {
 
                 <div> {/* Calendar date picker */}
                     <label for="eventDate">Event Date:</label>
-                    <input type="date" id="eventDate" name="eventDate" value="2020-02-20" min="2024-01-01" max="2026-12-31" />
+                    <input type="date" id="eventDate" name="eventDate" 
+                        min="2024-01-01" max="2026-12-31" required/>
+                </div>
+
+                <div>
+                    <input type='submit' value='Submit'/>
                 </div>
             </form>
         </div>
