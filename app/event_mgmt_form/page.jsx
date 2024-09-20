84f15b79783a20a3bbbaa257d71b855a44d489fb
jashourr <joshuleerod@gmail.com>
2024-09-19 22:21:10 -0500

    add event management page

---
+++ app/event_mgmt_form/page.jsx
@@ -0,0 +1,20 @@
+// import Image from "next/image";
+import './event_mgmt_form.css';
+
+const eventMgmtForm = () => {
+    return (
+        <div id = 'eventMgmt'>
+            <h1>Event Management</h1>
+            <hr/>
+            <form>
+                
+                <div className="inputbox">
+                    
+                </div>
+            </form>
+        </div>
+    );
+}
+
+
+export default eventMgmtForm
